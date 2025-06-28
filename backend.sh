121925cc686669da89a3e36639e732270384e90c
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-28 20:54:31 +0530

    common file

--- backend.sh
+++ backend.sh
@@ -5,13 +5,13 @@ source ./common.sh
 CHECK_USER
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATION_FUNCTION $? "diasbling node-js"
+#VALIDATION_FUNCTION $? "diasbling node-js"
 
 dnf module enable nodejs:20 -y  &>>$LOG_FILE
-VALIDATION_FUNCTION $? "enabling node-js"
+#VALIDATION_FUNCTION $? "enabling node-js"
 
 dnf install nodejs -y  &>>$LOG_FILE
-VALIDATION_FUNCTION $? "install  node-js"
+#VALIDATION_FUNCTION $? "install  node-js"
 
 # useradd expense &>>$LOG_FILE
 # VALIDATION_FUNCTION $? "creating expense user "
@@ -26,37 +26,37 @@ id expense &>>$LOG_FILE
     fi
 
 mkdir -p /app &>>$LOG_FILE
-VALIDATION_FUNCTION $? "creating app dir"
+#VALIDATION_FUNCTION $? "creating app dir"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
-VALIDATION_FUNCTION $? "downlode backend"
+#VALIDATION_FUNCTION $? "downlode backend"
 
 cd /app
 rm -rf /app/* 
 unzip /tmp/backend.zip &>>$LOG_FILE
-VALIDATION_FUNCTION $? "unzip the backend"
+#VALIDATION_FUNCTION $? "unzip the backend"
 
 npm install &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "downlode the dependencies"
+#VALIDATION_FUNCTION  $? "downlode the dependencies"
 
 cp /home/ec2-user/expense_shell_01/backend.service /etc/systemd/system/backend.service &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "copying the backend service"
+#VALIDATION_FUNCTION  $? "copying the backend service"
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "daemon -reload "
+#VALIDATION_FUNCTION  $? "daemon -reload "
 
 systemctl start backend &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "starting backend "
+#VALIDATION_FUNCTION  $? "starting backend "
 systemctl enable backend &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "enable backend "
+#VALIDATION_FUNCTION  $? "enable backend "
 
 dnf install mysql -y &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "installong mysql clint "
+#VALIDATION_FUNCTION  $? "installong mysql clint "
 
 mysql -h db.78skedar.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "schema loading"
+#VALIDATION_FUNCTION  $? "schema loading"
 
 systemctl restart backend &>>$LOG_FILE
-VALIDATION_FUNCTION  $? "restarting backed"
+#VALIDATION_FUNCTION  $? "restarting backed"
 
 
